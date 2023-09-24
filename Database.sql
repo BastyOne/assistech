92ebbe1bb23c1fb2cd957e44a69100501bf2522e
Bastian Pezo <92409327+BastyOne@users.noreply.github.com>
2023-09-23 21:27:12 -0300

    Update Database.sql

--- Database.sql
+++ Database.sql
@@ -23,7 +23,7 @@ CREATE TABLE roles (
 
 CREATE TABLE usuarios (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(50) NOT NULL,
+    rut VARCHAR(50) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
     rol_id INT,
     FOREIGN KEY (rol_id) REFERENCES roles(id)
@@ -31,7 +31,7 @@ CREATE TABLE usuarios (
 
 CREATE TABLE salas (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    codigo VARCHAR(20) NOT NULL,
+    codigo VARCHAR(20) NOT NULL UNIQUE,
     nombre VARCHAR(255),
     latitude DOUBLE,
     longitude DOUBLE,
@@ -47,8 +47,70 @@ CREATE TABLE asistencias (
     FOREIGN KEY (sala_id) REFERENCES salas(id)
 );
 
+CREATE TABLE materias (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(255) NOT NULL
+);
+
+
+CREATE TABLE profesor_materia (
+    profesor_id INT,
+    materia_id INT,
+    PRIMARY KEY (profesor_id, materia_id),
+    FOREIGN KEY (profesor_id) REFERENCES usuarios(id),
+    FOREIGN KEY (materia_id) REFERENCES materias(id)
+);
+
+CREATE TABLE clases_programadas (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    sala_id INT,
+    materia_id INT,
+    profesor_id INT,
+    fecha_hora TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (sala_id) REFERENCES salas(id),
+    FOREIGN KEY (materia_id) REFERENCES materias(id),
+    FOREIGN KEY (profesor_id) REFERENCES usuarios(id)
+);
+
+ALTER TABLE asistencias ADD COLUMN clase_programada_id INT;
+ALTER TABLE asistencias ADD FOREIGN KEY (clase_programada_id) REFERENCES clases_programadas(id);
+
+
+
+
+
+
+
+
+
+
+
 INSERT INTO roles (nombre) VALUES ('estudiante'), ('profesor');
 INSERT INTO roles (nombre) VALUES ('administrador');
+
+
+-- Insertar la sala 101 con coordenadas específicas y un radio de 10 metros
+INSERT INTO salas (codigo, nombre, latitude, longitude, radius) 
+VALUES ('S101', 'Sala 101', 40.7128, -74.0060, 10.0);
+
+-- Insertar la sala 102 con coordenadas diferentes y un radio de 15 metros
+INSERT INTO salas (codigo, nombre, latitude, longitude, radius) 
+VALUES ('S102', 'Sala 102', 34.0522, -118.2437, 15.0);
+
+-- Insertar la sala 103 con otras coordenadas y un radio de 20 metros
+INSERT INTO salas (codigo, nombre, latitude, longitude, radius) 
+VALUES ('S103', 'Sala 103', 51.5074, -0.1278, 20.0);
+
+
+
+INSERT INTO materias (nombre) 
+VALUES ('Gestion Informática');
+
+INSERT INTO materias (nombre) 
+VALUES ('Seguridad de la Informacion')
+
+INSERT INTO materias (nombre) 
+VALUES ('Taller de desarrollo de software');
 
 
 
@@ -56,11 +118,11 @@ UPDATE usuarios
 SET rol_id = 3
 WHERE id = id;
 
-Waren estudiante = colocolo1
-Bastian admin = colocolo
-Mondaca profe = 1234
+20519669 admin = colocolo1
+20565639 profesor = 1234
 
 select * from roles;
+select * from salas;
 select * from geofences;
 select * from estudiantes;
 select * from usuarios;
