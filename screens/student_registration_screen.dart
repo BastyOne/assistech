ef4f3c61c374898cf794824cbdc7aed1a9414e4a
Bastian Pezo <92409327+BastyOne@users.noreply.github.com>
2023-09-25 14:43:52 -0300

    Update student_registration_screen.dart

--- screens/student_registration_screen.dart
+++ screens/student_registration_screen.dart
@@ -1,5 +1,5 @@
 import 'dart:async';
-
+import 'package:assistech/screens/api_service.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
@@ -12,12 +12,14 @@ class GeoFenceArea {
   final double longitude;
   final double radius;
 
-  GeoFenceArea({required this.latitude, required this.longitude, required this.radius});
+  GeoFenceArea(
+      {required this.latitude, required this.longitude, required this.radius});
 }
 
 class StudentRegistrationScreen extends StatefulWidget {
-  const StudentRegistrationScreen({super.key});
-
+  final Map<String, dynamic> qrData;
+  const StudentRegistrationScreen({super.key, required this.qrData});
+  
   @override
   // ignore: library_private_types_in_public_api
   _StudentRegistrationScreenState createState() =>
@@ -28,20 +30,17 @@ class _StudentRegistrationScreenState extends State<StudentRegistrationScreen> {
   final TextEditingController _nombreController = TextEditingController();
   final TextEditingController _rutController = TextEditingController();
   final TextEditingController _correoController = TextEditingController();
-
+  final ApiService apiService = ApiService("http://192.168.100.81:3000", http.Client());
 
   StreamController<bool> geofenceStreamController = StreamController<bool>();
 
-  Future<bool> isInsideGeoFenceArea(Position position, GeoFenceArea geoFenceArea) async {   
-  double distanceInMeters = Geolocator.distanceBetween(
-    position.latitude, 
-    position.longitude, 
-    geoFenceArea.latitude, 
-    geoFenceArea.longitude
-  );
-  
-  return distanceInMeters <= geoFenceArea.radius;
-}
+  Future<bool> isInsideGeoFenceArea(
+      Position position, GeoFenceArea geoFenceArea) async {
+    double distanceInMeters = Geolocator.distanceBetween(position.latitude,
+        position.longitude, geoFenceArea.latitude, geoFenceArea.longitude);
+
+    return distanceInMeters <= geoFenceArea.radius;
+  }
 
   void monitorGeoFenceArea(GeoFenceArea geoFenceArea) async {
     Geolocator.getPositionStream().listen((Position position) async {
@@ -50,94 +49,118 @@ class _StudentRegistrationScreenState extends State<StudentRegistrationScreen> {
     });
   }
 
+  Future<void> _registrarEstudiante() async {
+    String nombre = _nombreController.text;
+    String rut = _rutController.text;
+    String correo = _correoController.text;
 
- Future<void> _registrarEstudiante() async {
-  String nombre = _nombreController.text;
-  String rut = _rutController.text;
-  String correo = _correoController.text;
+    LocationPermission permission = await Geolocator.checkPermission();
 
-  LocationPermission permission = await Geolocator.checkPermission();
-
-  if (permission == LocationPermission.denied) {
-    permission = await Geolocator.requestPermission();
-  }
-
-  if (permission == LocationPermission.denied) {
-    // Manejar el caso en que el usuario haya denegado el acceso a la ubicación
-  } else if (permission == LocationPermission.deniedForever) {
-    // Manejar el caso en que el usuario haya denegado permanentemente el acceso a la ubicación
-  } else {
-    final Position position = await Geolocator.getCurrentPosition(
-      desiredAccuracy: LocationAccuracy.high,
-      timeLimit: const Duration(seconds: 10),
-    );
-
-    GeoFenceArea ucmGeoFenceArea = GeoFenceArea(
-      latitude: -35.8691222, // reemplaza con la latitud de tu geofence
-      longitude: -71.5962363	, // reemplaza con la longitud de tu geofence
-      radius: 20.0, // reemplaza con el radio de tu geofence en metros
-    );
-
-    monitorGeoFenceArea(ucmGeoFenceArea);
-
-    Map<String, dynamic> studentData = {
-      'nombre': nombre,
-      'rut': rut,
-      'correo': correo,
-      'latitud': position.latitude,
-      'longitud': position.longitude,
-    };
-
-    final response = await http.post(
-      Uri.parse('http://192.168.100.81:3000/registrar-estudiante'),
-      headers: <String, String>{
-        'Content-Type': 'application/json; charset=UTF-8',
-      },
-      body: jsonEncode(studentData),
-    );
-
-    if (response.statusCode == 200) {
-      final responseData = jsonDecode(response.body);
-
-      if(responseData.containsKey('estudianteId')) {
-        final int estudianteId = responseData['estudianteId'];
-
-        final GeoFence newGeoFence = GeoFence(
-          name: 'UCM',
-          latitude: position.latitude,
-          longitude: position.longitude,
-          radius: 20.0,
-          estudianteId: estudianteId,  // Utilizando estudianteId aquí
-        );
-
-        await newGeoFence.registrarEnBaseDeDatos();
-
-        // Navegar a la pantalla de estado de geofencing
-        // ignore: use_build_context_synchronously
-        Navigator.push(
-          context,
-          MaterialPageRoute(
-            builder: (context) => GeofencingStatusScreen(
-              geofenceStream: geofenceStreamController.stream,
-            ),
-          ),
-        );
-      } else {
-        // Manejar el error de estudianteId no encontrado en la respuesta
-        print('estudianteId no encontrado en la respuesta');
-      }
-    } else {
-      // Manejar el error de registro de estudiante
-      print('Error al registrar estudiante: ${response.statusCode}');
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
     }
 
-    _nombreController.clear();
-    _rutController.clear();
-    _correoController.clear();
-  }
-}
+    if (permission == LocationPermission.denied) {
+      // Manejar el caso en que el usuario haya denegado el acceso a la ubicación
+    } else if (permission == LocationPermission.deniedForever) {
+      // Manejar el caso en que el usuario haya denegado permanentemente el acceso a la ubicación
+    } else {
+      final Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high,
+        timeLimit: const Duration(seconds: 10),
+      );
 
- @override
+      GeoFenceArea qrGeoFenceArea = GeoFenceArea(
+        latitude: widget.qrData['latitud'],
+        longitude: widget.qrData['longitud'],
+        radius: widget.qrData['radio'],
+      );
+
+      bool isInside = await isInsideGeoFenceArea(position, qrGeoFenceArea);      
+
+      if (!isInside) {
+    // Si el estudiante no está dentro del área geográfica, mostramos un mensaje y no continuamos con el proceso
+    showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+              title: Text("Error"),
+              content: Text(
+                  "No estás en la ubicación correcta para registrar tu asistencia."),
+              actions: [
+                TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: Text("Entendido"),
+                )
+              ],
+            ));
+    return; // Salimos del método sin registrar al estudiante
+  }
+
+
+      Map<String, dynamic> studentData = {
+        'nombre': nombre,
+        'rut': rut,
+        'correo': correo,
+        'latitud': position.latitude,
+        'longitud': position.longitude,
+      };
+
+      final response = await http.post(
+        Uri.parse('http://192.168.100.81:3000/registrar-estudiante'),
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+        },
+        body: jsonEncode(studentData),
+      );
+
+      if (response.statusCode == 200) {
+        final responseData = jsonDecode(response.body);
+
+        if (responseData.containsKey('estudianteId')) {
+          final int estudianteId = responseData['estudianteId'];         
+          final int claseProgramadaId = widget.qrData['clase_programada_id'];
+          final int salaId = widget.qrData['sala_id'];
+          final GeoFence newGeoFence = GeoFence(
+            name: 'UCM',
+            latitude: position.latitude,
+            longitude: position.longitude,
+            radius: 20.0,
+            estudianteId: estudianteId, // Utilizando estudianteId aquí
+          );
+
+          await newGeoFence.registrarEnBaseDeDatos();
+          await apiService.registrarAsistencia(estudianteId, claseProgramadaId, salaId);
+
+
+
+          // Navegar a la pantalla de estado de geofencing
+          // ignore: use_build_context_synchronously
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => GeofencingStatusScreen(
+                geofenceStream: geofenceStreamController.stream,
+              ),
+            ),
+          );
+        } else {
+          // Manejar el error de estudianteId no encontrado en la respuesta
+          print('estudianteId no encontrado en la respuesta');
+        }
+      } else {
+        // Manejar el error de registro de estudiante
+        print('Error al registrar estudiante: ${response.statusCode}');
+      }
+
+      _nombreController.clear();
+      _rutController.clear();
+      _correoController.clear();
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -156,6 +179,10 @@ class _StudentRegistrationScreenState extends State<StudentRegistrationScreen> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
+            Text(
+              'Latitud: ${widget.qrData['latitud']}, Longitud: ${widget.qrData['longitud']}',
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+            ),
             TextField(
               controller: _nombreController,
               decoration: const InputDecoration(labelText: 'Nombre'),
@@ -172,7 +199,8 @@ class _StudentRegistrationScreenState extends State<StudentRegistrationScreen> {
             Center(
               child: ElevatedButton(
                 style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all<Color>(Colors.black),
+                  backgroundColor:
+                      MaterialStateProperty.all<Color>(Colors.black),
                 ),
                 onPressed: _registrarEstudiante,
                 child: const Text(
