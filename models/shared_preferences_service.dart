e761797924d7b71bda76009a6d592458a2f3b6fd
Bastian Pezo <92409327+BastyOne@users.noreply.github.com>
2023-11-01 22:28:57 -0300

    Add files via upload

--- models/shared_preferences_service.dart
+++ models/shared_preferences_service.dart
@@ -2,19 +2,18 @@ import 'package:assistech/models/models.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class SharedPreferencesService {
-  
   Future<void> setUserDetails(UserModel user, RoleModel role) async {
     try {
       final prefs = await SharedPreferences.getInstance();
-      
+
       await prefs.setInt('userId', user.id);
       await prefs.setString('rut', user.rut);
       await prefs.setString('roleName', role.nombre);
-      
+
       print('User ID guardado: ${user.id}');
-      
     } catch (e) {
-      print('Error al guardar los detalles del usuario en SharedPreferences: $e');
+      print(
+          'Error al guardar los detalles del usuario en SharedPreferences: $e');
     }
   }
 
@@ -40,7 +39,49 @@ class SharedPreferencesService {
     await prefs.remove('userId');
     await prefs.remove('rut');
     await prefs.remove('roleName');
-    
+
     print('Detalles del usuario eliminados de SharedPreferences');
+  }
+
+  Future<void> setGeoFenceDetails(
+      double latitude, double longitude, double radius) async {
+    try {
+      final prefs = await SharedPreferences.getInstance();
+
+      await prefs.setDouble('geoFenceLatitude', latitude);
+      await prefs.setDouble('geoFenceLongitude', longitude);
+      await prefs.setDouble('geoFenceRadius', radius);
+
+      print('Detalles de GeoFence guardados');
+    } catch (e) {
+      print(
+          'Error al guardar los detalles de GeoFence en SharedPreferences: $e');
+    }
+  }
+
+  Future<Map<String, dynamic>> getGeoFenceDetails() async {
+    final prefs = await SharedPreferences.getInstance();
+
+    double? latitude = prefs.getDouble('geoFenceLatitude');
+    double? longitude = prefs.getDouble('geoFenceLongitude');
+    double? radius = prefs.getDouble('geoFenceRadius');
+
+    print('Detalles de GeoFence recuperados');
+
+    return {
+      'latitude': latitude,
+      'longitude': longitude,
+      'radius': radius,
+    };
+  }
+
+  Future<void> clearGeoFenceDetails() async {
+    final prefs = await SharedPreferences.getInstance();
+
+    await prefs.remove('geoFenceLatitude');
+    await prefs.remove('geoFenceLongitude');
+    await prefs.remove('geoFenceRadius');
+
+    print('Detalles de GeoFence eliminados de SharedPreferences');
   }
 }
